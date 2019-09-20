e48be99074c418f32837b6bf419151b635cd0ce2
AllisonW2323 <54549422+AllisonW2323@users.noreply.github.com>
2019-09-20 01:59:00 -0400

    Update frankenpoem.py

--- poem/frankenpoem.py
+++ poem/frankenpoem.py
@@ -1,41 +1,95 @@
+from google.colab import files
 import random
+
+uploaded = files.upload()
+for fn in uploaded.keys():
+  text = uploaded[fn].decode()
+  
+wordlist= text.split(" ")
 from textblob import TextBlob
 
-# This looks for a file called "frankenstein.txt" 
-# in the same directory as my python notebook or script.
-# Replace "frankenstein.txt" with your file's name.
-# The text file's contents are stored in the variable "text"
-# These are comments
-with open('frankenstein.txt', 'r') as file:
-    text = file.read()
+blob= TextBlob (text)
 
-# This parses the text file contents into a TextBlob oject called "blob"
-blob = TextBlob(text)
+random.randint(1,6)
+!pip install textblob
+from textblob import TextBlob
 
-# Create two empty lists for storing adjectives and nouns
-adjectives = []
-nouns = []
-verbs = []
+blob= TextBlob (text)
 
-# TextBlob parses the words and labels them with a part-of-speech tag.
-# This code loops through the whole text, checks for adjectives and nouns
-# and adds them to the appropriate list.
-for word,pos in blob.tags:
-    # print(word,pos)
-    if (pos == 'JJ'):
-        adjectives.append(word)
-    if (pos == 'NN'):
-        nouns.append(word)
-    if (pos == 'VB'):
-        verbs.append(word)
+import nltk
 
-# This generates an eight-line poem by pairing a random adjective 
-# with a random noun eight times and printing the pairs.
-for i in range(8):
-    a1 = random.choice(adjectives)
-    a2 = random.choice(adjectives)
-    n1 = random.choice(nouns)
-    n2 = random.choice(nouns)
-    n3 = random.choice(nouns)
-    vb = random.choice(verbs)
-    print(n1 + " " + a1 + " " +  vb  + " " + a2  + " " + n3)
+nltk.download('averaged_perceptron_tagger')
+
+import nltk
+nltk.download('punkt')
+
+
+Bannedword=["chink", "Chink", "Niger", "niger"]   #had to write them so my bot would not
+
+
+
+adjlist=[]
+for word,tag in blob.tags:
+  if tag == "JJ":
+   adjlist.append(word)
+    
+for i in range (1):
+  word1=(adjlist[random.randint(0,len(adjlist)-1)])
+  if word1 not in Bannedword:
+    print(word1)
+  else:
+    print("bleep")
+    
+    
+nounlist=[]
+for word,tag in blob.tags:
+  if tag == "NN":
+    nounlist.append(word)
+    
+for i in range (1):
+  word1=(nounlist[random.randint(0,len(nounlist)-1)])
+  if word1 not in Bannedword:
+    print(word1)
+  else:
+    print("bleep")
+    
+    
+verblist=[]
+for word,tag in blob.tags:
+  if tag == "VB":
+    verblist.append(word)
+    
+for i in range (1):
+  word1=(verblist[random.randint(0,len(verblist)-1)])
+  if word1 not in Bannedword:
+    print(word1)
+  else:
+    print("bleep")
+
+for i in range(1):
+    print (blob.sentences[random.randint (0,len(blob.sentences))])
+    
+verblist=[]
+for word,tag in blob.tags:
+  if tag == "VB":
+    verblist.append(word)
+    
+for i in range (1):
+  word1=(verblist[random.randint(0,len(verblist)-1)])
+  if word1 not in Bannedword:
+    print(word1)
+  else:
+    print("bleep")
+  
+nounlist=[]
+for word,tag in blob.tags:
+  if tag == "NN":
+    nounlist.append(word)
+    
+for i in range (1):
+  #print(nounlist[random.randint(0,len(nounlist)-1)]) 
+  word1=(nounlist[random.randint(0,len(nounlist)-1)])
+  if word1 not in Bannedword:
+    print(word1)
+  else:
+    print("bleep")
